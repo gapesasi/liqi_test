4af39abbecee58c4540a4584198f2c7b4cf88ec8
Gabriel Santana <gabrielpe1928@gmail.com>
2025-04-04 12:42:55 -0300

    feat: add transaction service consumer

---
+++ src/domain/services/TransactionService/consumer.ts
@@ -0,0 +1,45 @@
+import { eventEmitter } from "../../events/EventEmitter";
+import { TransactionEvent, TransactionEventData } from "../../events/TransactionEvents";
+import { transactionQueue } from "../SimpleQueueService/TransactionQueue";
+import { TransactionEventPayload } from "./types";
+
+const transactionQueueConsumer = async () => {
+  await transactionQueue.createQueue();
+
+  const poll = async () => {
+    while (true) {
+      try {
+        const transactions = await transactionQueue.unqueue();
+
+        console.log("Transactions Found: ", transactions);
+
+        if (transactions.length > 0) {
+          transactions.forEach((transaction) => {
+            const transacionEvent: TransactionEventData<TransactionEventPayload> = {
+              event: TransactionEvent.PROCESSING_STARTED,
+              data: {
+                ...transaction.message,
+                timestamp: new Date(),
+                status: "pending",
+                messageReceiptHandle: transaction.receiptHandle,
+                event: TransactionEvent.PROCESSING_STARTED,
+              },
+            };
+
+            eventEmitter.emit(transacionEvent.event, transacionEvent.data);
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+
+      await new Promise<void>((res) => {
+        setTimeout(res, 50);
+      });
+    }
+  };
+
+  await poll();
+};
+
+export default transactionQueueConsumer;
---
+++ src/domain/services/TransactionService/types.ts
@@ -0,0 +1,16 @@
+import { TransactionEvent } from "../../events/TransactionEvents";
+
+export type TransactionPayload = {
+  transaction_id: string;
+  value: number;
+  type: "debit" | "credit";
+  origin: string;
+  target: string;
+};
+
+export type TransactionEventPayload = TransactionPayload & {
+  timestamp: Date;
+  status: "pending" | "processing" | "completed" | "failed";
+  messageReceiptHandle: string;
+  event: TransactionEvent;
+};
